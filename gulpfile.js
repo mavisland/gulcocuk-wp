f522728e2ece142e89028b8a0017390e5d91648f
Tanju Yildiz <ben@tanjuyildiz.com>
2017-09-06 11:51:30 +0300

    gulpfile.js duzenlemesi yapildi. src klasorunde degisiklikler yapildi.

--- gulpfile.js
+++ gulpfile.js
@@ -7,70 +7,93 @@
 
 'use strict';
 
-// Required packages
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
-var autoPrefixer = require('gulp-autoprefixer');
-var cache        = require('gulp-cache');
-var cleanCss     = require('gulp-clean-css');
-var concat       = require('gulp-concat');
-var header       = require('gulp-header');
-var imageMin     = require('gulp-imagemin');
-var jshint       = require('gulp-jshint');
+/**
+ * Load Plugins.
+ *
+ * Load gulp plugins and passing them semantic names.
+ */
+var gulp = require('gulp');
+
+// CSS related plugins.
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS     = require('gulp-clean-css');
 var less         = require('gulp-less');
-var notify       = require('gulp-notify');
-var plumber      = require('gulp-plumber');
-var reload       = browserSync.reload;
-var rename       = require('gulp-rename');
-var uglify       = require('gulp-uglify');
+
+// JS related plugins.
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+// Image realted plugins.
+var cache    = require('gulp-cache');
+var imageMin = require('gulp-imagemin');
+
+// Utility related plugins.
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var header      = require('gulp-header');
+var gutil       = require('gulp-util');
+var notify      = require('gulp-notify');
+var plumber     = require('gulp-plumber');
+var rename      = require('gulp-rename');
 
 // If node version is lower than v.0.1.2
 require('es6-promise').polyfill();
 
 // Set the banner content
 var banner = ['/*',
-  ' Theme Name:    Atiker Konyaspor',
-  ' Theme URI:     http://www.konyaspor.org.tr/',
-  ' Description:   Atiker Konyaspor Web Sitesi',
-  ' Version:       1.0.0',
-  ' Author:        Tanju Yildiz <tanju@themediatix.com>',
-  ' Author URI:    http://themediatix.com/',
+  ' Theme Name:    Gulcocuk Anaokulu',
+  ' Theme URI:     http://gulcocukanaokulu.k12.tr/',
+  ' Description:   Gulcocuk Anaokulu Web Sitesi',
+  ' Version:       1.1.0',
+  ' Author:        Tanju Yildiz <ben@tanjuyildiz.com>',
+  ' Author URI:    http://weblebix.com/',
   ' */',
   ''].join('\n');
 
-// Browsers you care about for autoprefixing.
-// Browserlist https://github.com/ai/browserslist
-var AUTOPREFIXER_BROWSERS = [
-  'last 2 version',
-  '> 1%',
-  'ie >= 9',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4',
-  'bb >= 10'
-];
+/**
+ * Task: `browser-sync`.
+ *
+ * Live Reloads, CSS injections, Localhost tunneling.
+ */
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    // For more options
+    // @link http://www.browsersync.io/docs/options/
 
-// Compile LESS files from /less into /css
-gulp.task('less',function(){
-  gulp.src(['src/styles/**/*.less'])
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
+    // Project URL.
+    proxy: 'gulcocukanaokulu.dev',
+
+    // `true` Automatically open the browser with BrowserSync live server.
+    // `false` Stop the browser from automatically opening.
+    open: true,
+
+    // Inject CSS changes.
+    // Commnet it to reload browser for every CSS change.
+    injectChanges: true,
+
+    // Use a specific port (instead of the one auto-detected by Browsersync).
+    // port: 7000,
+  } );
+});
+
+/**
+ * Task: `styles`.
+ *
+ * Compiles Less, Autoprefixes it and Minifies CSS.
+ */
+gulp.task('styles', function(){
+  return gulp.src(['src/styles/style.less'])
+    .pipe(plumber(function(error) {
+      // Output an error message
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      // emit the end event, to properly end the task
+      this.emit('end');
     }))
     .pipe(less({
       paths: ["."] // Specify search paths for @import directives
     }))
-    .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('./'))
-    .pipe(rename({
-      suffix: '.min'
-    }))
     // Minify compiled CSS
     .pipe(cleanCSS({
       compatibility: 'ie8',
@@ -78,22 +101,37 @@ gulp.task('less',function(){
     }))
     .pipe(header(banner))
     .pipe(gulp.dest('./'))
-    .pipe(reload())
-    .pipe(notify('Compiled LESS files to CSS files.'))
+    .pipe(reload({stream:true}))
+    .pipe(notify({
+      message: 'TASK: "styles" Completed! 💯',
+      onLast: true
+    }))
 });
 
-// Concatenates JS files and minified
-gulp.task('js', function(){
-  gulp.src(['src/scripts/**/*.js'])
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
+ /**
+  * Task: `scripts`.
+  *
+  * Concatenate and uglify vendor JS scripts.
+  */
+gulp.task('scripts', function(){
+  gulp.src([
+    './src/components/bootstrap/js/bootstrap.min.js',
+    './src/components/owl-carousel/owl.carousel.min.js',
+    './src/components/fancybox/jquery.fancybox.pack.js',
+    './src/components/isotope/isotope.min.js',
+    './src/components/isotope/isotope-triger.min.js',
+    './src/components/countdown/jquery.syotimer.js',
+    './src/components/selectbox/jquery.selectbox-0.1.3.min.js',
+    './src/components/smoothscroll/SmoothScroll.min.js',
+    './src/scripts/theme-custom.js'
+  ])
+    .pipe(plumber(function(error) {
+      // Output an error message
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      // emit the end event, to properly end the task
+      this.emit('end');
     }))
     .pipe(concat('scripts.js'))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
     .pipe(gulp.dest('./js/'))
     .pipe(rename({
       suffix: '.min'
@@ -101,44 +139,83 @@ gulp.task('js', function(){
     // Minify JS
     .pipe(uglify())
     .pipe(gulp.dest('./js/'))
-    .pipe(reload())
-    .pipe(notify('Minified JS files.'))
-});
-
-gulp.task('html',function(){
-  gulp.src(['html/**/*.html'])
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
+    .pipe(reload({stream:true}))
+    .pipe(notify({
+      message: 'TASK: "scripts" Completed! 💯',
+      onLast: true
     }))
-    .pipe(gulp.dest('./'))
-    .pipe(reload())
-    .pipe(notify('html task finished'))
 });
 
-gulp.task('image',function(){
+/**
+  * Task: `images`.
+  *
+  * Minifies PNG, JPEG, GIF and SVG images.
+  *
+  * This task will run only once, if you want to run it
+  * again, do it with the command `gulp images`.
+  */
+gulp.task('images', function(){
   gulp.src(['src/images/**/*'])
-    .pipe(plumber({
-      handleError: function (err) {
-        console.log(err);
-        this.emit('end');
-      }
+    .pipe(plumber(function(error) {
+      // Output an error message
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      // emit the end event, to properly end the task
+      this.emit('end');
     }))
-    .pipe(cache(imageMin()))
+    .pipe(cache(imageMin({
+      progressive: true,
+      optimizationLevel: 3, // 0-7 low-high
+      interlaced: true,
+      svgoPlugins: [{removeViewBox: false}]
+    })))
     .pipe(gulp.dest('./images'))
-    .pipe(reload())
-    .pipe(notify('image task finished'))
+    .pipe(reload({stream:true}))
+    .pipe(notify({
+      message: 'TASK: "images" Completed! 💯',
+      onLast: true
+    }))
 });
 
-// Run everything
-gulp.task('default',function(){
-  browserSync.init({
-    server: "./"
-  });
-  gulp.watch('src/scripts/**/*.js', ['js']);
-  gulp.watch('src/styles/**/*.less', ['less']);
-  gulp.watch('html/**/*.html', ['html']);
-  gulp.watch('src/images/**/*', ['image']);
+// Copy vendor files from 'components' into 'dist'
+gulp.task('copy', function(){
+  // Vendor Javascript files
+  gulp.src([
+    './src/components/jquery/jquery.min.js',
+    './src/components/jquery-ui/jquery-ui.min.js',
+    './src/components/html5shiv/html5shiv.min.js',
+    './src/components/respond/respond.min.js',
+  ])
+    .pipe(gulp.dest('./scripts'));
+
+  // Vendor assets files
+  gulp.src([
+    "src/components/fancybox/images/*.{png,jpg,gif}",
+    "src/components/jquery-ui/images/*.{png,jpg,gif}",
+    "src/components/owl-carousel/images/*.{png,jpg,gif}",
+    "src/components/seletbox/images/*.{png,jpg,gif}"
+  ])
+    .pipe(gulp.dest("./images"));
+
+  // Custom fonts
+  gulp.src([
+    'src/components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}',
+    'src/components/font-awesome/fonts/*.{eot,svg,ttf,woff,woff2}'
+  ])
+    .pipe(gulp.dest('./fonts'));
+});
+
+/**
+  * Watch Tasks.
+  *
+  * Watches for file changes and runs specific tasks.
+  */
+gulp.task('default', ['styles', 'scripts', 'images', 'browser-sync'], function(){
+  // Reload on PHP file changes.
+  gulp.watch('./**/*.php', reload);
+
+  // Reload on LESS file changes.
+  gulp.watch('src/styles/**/*.less', ['styles']);
+
+  // Reload on vendorsJs file changes.
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
