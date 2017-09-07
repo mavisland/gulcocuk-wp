2f4ba7203b5810239fafcf29a1e6abd005897ce8
Tanju Yildiz <ben@tanjuyildiz.com>
2017-09-07 23:44:07 +0300

    Slider alani duzenlendi.

---
+++ template-parts/home-main-slider.php
@@ -0,0 +1,23 @@
+	<section class="main-slider" data-loop="true" data-autoplay="true" data-interval="7000">
+		<div class="inner">
+			<div class="slide slide1" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/banner/slider-1.jpg);">
+				<div class="container">
+					<div class="slide-inner1 common-inner">
+						<span class="h1 from-bottom">Get kidkite <br><span class="color-2">For Your Store</span></span><br>
+						<span class="h4 from-bottom">kidkite Template has all the features need to run an ecommerce website.</span><br>
+						<a href="#" class="btn btn-main first-btn waves-effect waves-light scale-up">Purchase Now</a>
+					</div>
+				</div>
+			</div>
+			<div class="slide slide3" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/banner/slider-2.jpg);">
+				<div class="container">
+					<div class="slide-inner3 common-inner">
+						<span class="h1 from-bottom">Get Kidkite <br><span class="color-2">For Your Store</span></span><br>
+						<span class="h4 from-bottom">kidkite Template has all the features need to run an ecommerce website.</span><br>
+						<a href="#" class="btn btn-main first-btn waves-effect waves-light scale-up">Purchase Now</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</section>
